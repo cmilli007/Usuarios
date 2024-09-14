314d6f7b9fc90d1a28e83f13a002ccba590bceb2
camili <camili.costa07@gmail.com>
2024-09-14 20:32:23 -0300

    testar as duas telas

--- cadastrar.php
+++ cadastrar.php
@@ -8,50 +8,54 @@ $conn = new mysqli($servidor, $user, $password $bd);
 if(!$conn){
 
  echo "<p style=' color:red; text-align:center; font-size:25px;'Erro de conexão!>/p>";
- echo"<h2 style = 'color:green; text-align:center; font-size:25px'><a href= 'usuario.html'>VOLTAR<a></h2>";
+ echo"<h2 style = 'color:green; text-align:center; font-size:25px'><a href= 'usuarios_empresas.html'>VOLTAR<a></h2>";
 
 }
 
 if($_SERVER ["REQUEST_METHOD"=="POST"]){
  
-    $nm_cliente = $_POST ["nm_cliente"];
-    $email = $_POST ["email"];
+    $nm_empresa = $_POST ["nm_empresa"];
+    $cnpj_empresa = $_POST ["cnpj_empresa"];
     $nm_endereco = $_POST ["nm_endereco"];
     $nr_endereco = $_POST ["nr_endereco"];
     $nr_cep = $_POST ["nr_cep"];
-    $nr_telefone = $_POST ["nr_telefone"];
+    $email = $_POST ["email"];
+    $nm_responsavel = $_POST ["nm_responsavel"];
+    $cargo_responsavel = $_POST ["cargo_responsavel"];
+    $email_responsavel = $_POST ["email_responsavel "];
+    $ds_serico = $_POST ["ds_serico"];
     $senha = $_POST ["senha"];
     $ConfirmaSenha = $_POST ["ConfirmaSenha"];
 
     if($senha ====$ConfirmaSenha){
 
-        $sql = "SELECT * FROM  tb_vendedores WHERE nm_cliente ='$nm_cliente'";
+        $sql = "SELECT * FROM  tb_empresa WHERE nm_empresa ='$nm_empresa'";
         $retorno = mysqli_query ($conn, $sql);
         $row =mysqli_fetch_assoc ($retorno);
 
         if($row){
 
             echo "<p style=' color:red; text-align:center; font-size:25px;'Usuário Já Existe!>/p>";
-            echo"<h2 style = 'color:green; text-align:center; font-size:25px'><a href= 'usuario.html'>VOLTAR<a></h2>";
+            echo"<h2 style = 'color:green; text-align:center; font-size:25px'><a href= 'usuarios_empresas.html'>VOLTAR<a></h2>";
         } else {
 
             $hashsenha = password_hash($senha, PASSWORD_BCRYPT);
-            $sql = "INSERT INTO  tb_vendedores (nm_cliente, senha) values ('$nm_cliente' ,  '$hashsenha')";
+            $sql = "INSERT INTO  tb_vendedores (nm_cliente, senha) values ('$nm_empresa' ,  '$hashsenha')";
         $retorno = mysqli_query ($conn, $sql);
         
         if($retorno ===true){
     echo "<p style=' color:green; text-align:center; font-size:25px;'Cadastro Realizado!>/p>";
-    echo"<h2 style = 'color:green; text-align:center; font-size:25px'><a href= 'usuario.html'>VOLTAR<a></h2>";
+    echo"<h2 style = 'color:green; text-align:center; font-size:25px'><a href= 'usuarios_empresas.html'>VOLTAR<a></h2>";
 } else {
     echo "ERRO AO CADASTRAR USUÁRIO:". $conn->error;
-    echo"<h2 style = 'color:green; text-align:center; font-size:25px'><a href= 'usuario.html'>VOLTAR<a></h2>";
+    echo"<h2 style = 'color:green; text-align:center; font-size:25px'><a href= 'usuarios_empresas.html'>VOLTAR<a></h2>";
 }
 
 
         }
 } else {
     echo "<p style=' color:red; text-align:center; font-size:25px;'As senhas não coincidem !>/p>";
-    echo"<h2 style = 'color:green; text-align:center; font-size:25px'><a href= 'usuario.html'>VOLTAR<a></h2>";
+    echo"<h2 style = 'color:green; text-align:center; font-size:25px'><a href= 'usuarios_empresas.html'>VOLTAR<a></h2>";
 }
 
 }
